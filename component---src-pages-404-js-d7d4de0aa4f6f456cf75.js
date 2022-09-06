5a5b06dd1449b7fdaf62c0d1ac49808ec70a1942
ruarfff <ruairitobrien@gmail.com>
2022-09-06 23:14:21 +0000

    Deploying to main from @ ruarfff/ruarfff-blog@f7e8c93f56986a8f4b9cfb1bb1d2a9604d2a23f4 🚀

---
+++ component---src-pages-404-js-d7d4de0aa4f6f456cf75.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkruarfff_blog=self.webpackChunkruarfff_blog||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),l=a(8678),u=a(262);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d7d4de0aa4f6f456cf75.js.map
